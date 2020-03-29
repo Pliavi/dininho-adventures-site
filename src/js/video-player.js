4671ab0da28f4e0e655a83b7b129cf21b0672dff
Pliavi <pliavi.rpgm@gmail.com>
2020-03-29 15:04:54 -0300

    add vide oplayer

---
+++ src/js/video-player.js
@@ -0,0 +1,17 @@
+/** @type [HTMLMediaElement] */
+const $playerWrappers = document.getElementsByClassName("video-player");
+
+
+for (const $playerWrapper of $playerWrappers) {
+  const $player = $playerWrapper.querySelector("video")
+  $playerWrapper.addEventListener("click", () => {
+    if($player.paused) {
+      $player.play()
+      $playerWrapper.classList.add("--playing")
+      return 
+    } 
+    
+    $player.pause()
+    $playerWrapper.classList.remove("--playing")
+  });
+}
