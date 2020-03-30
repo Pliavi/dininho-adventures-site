5e5cee687f62bc9b8b7ca4d8d810369367364749
Pliavi <pliavi.rpgm@gmail.com>
2020-03-30 19:35:19 -0300

    make paralax and player IIF's

--- src/js/paralax.js
+++ src/js/paralax.js
@@ -1,8 +1,10 @@
-const $paralax = document.getElementById("paralax");
+(function() {
+  const $paralax = document.getElementById("paralax");
 
-$paralax.style.setProperty("--paralax-move-x", "0px");
-$paralax.style.setProperty("--paralax-move-y", "0px");
-$paralax.addEventListener("mousemove", e => {
-  $paralax.style.setProperty("--paralax-move-x", `${e.clientX * 0.025}px`);
-  $paralax.style.setProperty("--paralax-move-y", `${e.clientY * 0.025}px`);
-});
+  $paralax.style.setProperty("--paralax-move-x", "0px");
+  $paralax.style.setProperty("--paralax-move-y", "0px");
+  $paralax.addEventListener("mousemove", e => {
+    $paralax.style.setProperty("--paralax-move-x", `${e.clientX * 0.025}px`);
+    $paralax.style.setProperty("--paralax-move-y", `${e.clientY * 0.025}px`);
+  });
+})();
--- src/js/video-player.js
+++ src/js/video-player.js
@@ -1,17 +1,18 @@
-/** @type [HTMLMediaElement] */
-const $playerWrappers = document.getElementsByClassName("video-player");
+(function() {
+  /** @type [HTMLMediaElement] */
+  const $playerWrappers = document.getElementsByClassName("video-player");
 
+  for (const $playerWrapper of $playerWrappers) {
+    const $player = $playerWrapper.querySelector("video");
+    $playerWrapper.addEventListener("click", () => {
+      if ($player.paused) {
+        $player.play();
+        $playerWrapper.classList.add("--playing");
+        return;
+      }
 
-for (const $playerWrapper of $playerWrappers) {
-  const $player = $playerWrapper.querySelector("video")
-  $playerWrapper.addEventListener("click", () => {
-    if($player.paused) {
-      $player.play()
-      $playerWrapper.classList.add("--playing")
-      return 
-    } 
-    
-    $player.pause()
-    $playerWrapper.classList.remove("--playing")
-  });
-}
+      $player.pause();
+      $playerWrapper.classList.remove("--playing");
+    });
+  }
+})();
