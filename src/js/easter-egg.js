b6bd9c6fd34131b93231d3f434d66bdcd0f7abfe
Pliavi <pliavi.rpgm@gmail.com>
2020-03-30 19:34:50 -0300

    removing console.logs from easter egg

--- src/js/easter-egg.js
+++ src/js/easter-egg.js
@@ -1,20 +1,20 @@
 (function() {
   let eggCounter = 0;
+
   document.querySelectorAll(".separator").forEach($egg => {
     $egg.addEventListener("click", checkEggCount);
   });
 
-  function checkEggCount(e) {
+  function checkEggCount({currentTarget: $egg}) {
     eggCounter += 1
     if (eggCounter > 3) {
       showKey();
     }
-    console.log(e.currentTarget, eggCounter)
-    e.currentTarget.removeEventListener("click", checkEggCount);
+    
+    $egg.removeEventListener("click", checkEggCount);
   }
 
   function showKey() {
-    console.log("show!")
     const $easterEgg = document.getElementById("easter-egg");
     const $keyMover = document.getElementById("key-mover");
 
@@ -23,6 +23,6 @@
 
     setTimeout(() => {
       $easterEgg.style.display = "none";
-    }, 4000); // this 4s is from 3s delay and 1s animation in css
+    }, 4000); // XXX: this 4s is from 3s delay and 1s animation in css
   }
 })();
